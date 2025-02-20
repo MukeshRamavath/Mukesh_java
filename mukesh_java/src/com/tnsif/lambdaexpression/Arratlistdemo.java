1b01a84f8e07bdd226a10f49b4619ddcbe8d6ca6
ramav <ramav@Mukesh>
2025-02-20 11:32:56 +0530

    first

---
+++ mukesh_java/src/com/tnsif/lambdaexpression/Arratlistdemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.lambdaexpression;
+
+import java.util.ArrayList;
+
+public class Arratlistdemo {
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> a=new ArrayList<Integer>();
+		a.add(2);
+		a.add(5);
+		a.add(7);
+		a.add(8);
+		
+		a.forEach(List->System.out.println(List));
+	}
+}
